6565a5ded4f80eb60a2666809b7b856ea067f7c3
chibaofang <435687248@qq.com>
2014-11-25 19:55:16 +0800

    hello,world

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+//using namespace std; do std declaration necessary?
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(){
+	cout<<"please say something:\n";
+	char buffer[100];
+	cin>>buffer[100];
+	cout<<"hello,world!"<<endl;
+	return 0;
+}
