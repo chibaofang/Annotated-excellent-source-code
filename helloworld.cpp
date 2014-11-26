168094254f4cfb3465265582f00f3492df35ebaa
chibaofang <435687248@qq.com>
2014-11-27 00:32:40 +0800

    to check the size of int, long, ...

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,16 +1,22 @@
 #include<iostream>
 #include<string>
-//using namespace std; do std declaration necessary?
+#include<vector>
+using namespace std;//here this sentence is necessary.(I do not know why...). Without it, compiler would say that "str" was not declared.
 using std::cin;
 using std::cout;
 using std::endl;
 int main(){
-	string str = "hello";
 	cout<<"int ="<<sizeof(int)<<"\n";
 	cout<<"long = "<<sizeof(long)<<"\n";
 	cout<<"float = "<<sizeof(float)<<"\n";
 	cout<<"double = "<<sizeof(double)<<"\n";
 	cout<<"char = "<<sizeof(char)<<"\n";
-	cout<<"str = "<<sizeof(str)<<endl;
+	cout<<"str = "<<sizeof(string)<<"\n";
+	vector<int> a(1);
+	vector<float> b(1);
+	vector<double> c(1);
+	cout<<"vector<int> = "<<sizeof(a)<<"\n";
+	cout<<"vector<float> = "<<sizeof(b)<<"\n";
+	cout<<"vector<char> = "<<sizeof(c)<<endl;
 	return 0;
 }
